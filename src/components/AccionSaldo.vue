897a25bf4737bbb71329fd521556725fbcf2d474
Sergio C <sergiocalvosos@gmail.com>
2020-10-09 12:56:15 +0200

    Cambiando a PC

---
+++ src/components/AccionSaldo.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="this.$emit('accion')">{{texto}}</button>
+</template>
+
+<script>
+
+export default {
+    props:{
+        texto: String
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Cuenta.vue
@@ -0,0 +1,46 @@
+<template>
+  <h2>  Tipo de cuenta: {{ cuenta }} </h2>
+  <h2>  Saldo: {{saldo}} </h2>
+  <h2>  Cuenta {{estado?'Activa':'Desactivada' }} </h2>
+  <div v-for="(servicio, index) in servicios" :key="index">
+     {{index +1}} - {{ servicio }}
+  </div>
+  <AccionSaldo 
+  texto="Aumentar Saldo"
+  @accion="aumentar"
+  />
+  <AccionSaldo 
+  texto="Disminuir Saldo"
+  @accion="disminuir"
+  />
+</template>
+
+<script>
+import AccionSaldo from './AccionSaldo'
+export default {
+    components:{
+        AccionSaldo
+    },
+    name:'Cuenta',
+    data() {
+        return {
+            saldo: 1000,
+            cuenta:'Visa',
+            estado:true,
+            servicios:['giro','abono','transferencia']
+        }
+    },
+    methods:{
+        aumentar(){
+            this.saldo = this.saldo + 100
+        },
+        disminuir(){
+            this.saldo = this.saldo - 100
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
